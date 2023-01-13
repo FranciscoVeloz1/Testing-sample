17422e072ef53a197778c628a042513f2a7ee06a
FranciscoVeloz <franciscoveloz245@gmail.com>
2023-01-13 13:24:38 -0600

    Agregamos api al index

--- index.test.js
+++ index.test.js
@@ -1,18 +1,29 @@
-const api = require("./api");
+const api = async (body) => {
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.parse(body),
+    });
+    return await res.json();
+  } catch (error) {
+    return "error";
+  }
+};
 
 describe("Muchos testings", () => {
   test("API 1", async () => {
-    const res = await api(1);
-    expect(res.id).toBe(1);
+    const res = await api({ username: "Francisco", password: "Hello" });
+    expect(res.status).toBe(200);
   });
 
   test("API 2", async () => {
-    const res = await api(2);
-    expect(res.id).toBe(2);
+    const res = await api({ username: "Francisco2", password: "Hello2" });
+    expect(res.status).toBe(200);
   });
 
   test("API 10", async () => {
-    const res = await api(10);
-    expect(res.id).toBe(10);
+    const res = await api({ username: "Francisco3", password: "Hello3" });
+    expect(res.status).toBe(200);
   });
 });
